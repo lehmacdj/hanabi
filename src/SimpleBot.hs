8ac8932c2fd0e45b2a2c847edc6afe9241665b94
Devin Lehmacher <lehmacdj@gmail.com>
2020-05-10 16:26:09 -0700

    write some code for the bot

--- src/SimpleBot.hs
+++ src/SimpleBot.hs
@@ -7,18 +7,28 @@ import Game
 
 -- | the information that a player can have about the game
 data PlayerInformation p = PlayerInformation
-    { handPossibilities :: HandPossibilities
-    -- ^ the possibilities that this player thinks their hand could be
-    , perspectiveMap :: PlayerMap p (PlayerInformation p)
-    -- ^ the information this player thinks that other players know
-    -- if the info is for player p then the following is an invariant
-    -- > perspectiveMap . at p == id
-    }
-    deriving (Show, Generic)
+  { handPossibilities :: HandPossibilities
+  -- ^ the possibilities that this player thinks their hand could be
+  , perspectiveMap :: PlayerMap p (PlayerInformation p)
+  -- ^ the information this player thinks that other players know
+  -- if the info is for player p then the following is an invariant
+  -- > perspectiveMap . at p == id
+  }
+  deriving (Show, Generic)
+
+updatePlayerInfo
+  :: Ord p => Information p -> PlayerInformation p -> PlayerInformation p
+updatePlayerInfo i pi = case i of
+    RemovedFromHand p cix -> undefined
+    CardSatisfies p cix ci -> undefined
 
 data BotInformation p = BotInformation
-    { player :: p
-    , playerInformation :: PlayerInformation p
-    , cardsRemaining :: Set Card
-    }
-    deriving (Show, Generic)
+  { player :: p
+  , playerInformation :: PlayerInformation p
+  , cardsRemaining :: Set Card
+  }
+  deriving (Show, Generic)
+
+updateBotInfo
+  :: Ord p => Information p -> BotInformation p -> BotInformation p
+updateBotInfo i = over #playerInformation (updatePlayerInfo i)
