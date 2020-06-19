66ec2b077a1090ae9b3adddb6b62ecfd3550498a
Devin Lehmacher <lehmacdj@gmail.com>
2020-06-18 22:42:45 -0700

    implement first pass on console executable

--- app/console/Main.hs
+++ app/console/Main.hs
@@ -1,6 +1,60 @@
 module Main where
 
+import ConsoleIO
+import ConsoleUI
+import Data.Function ((&))
+import Data.Random
+import Game
 import MyPrelude
+import Polysemy.Error
+import Polysemy.Output
+import System.Console.Haskeline
+import ThreePlayer
+
+printEndgameResult :: Member ConsoleIO r => GameState p -> Sem r ()
+printEndgameResult state = do
+  writeln "The game has ended!"
+  writeln "Your score is:"
+  for_ (state ^@.. #board . #fireworks . #underlyingMap . itraversed) $
+    \(c, n) -> writeln $ show c ++ " => " ++ show n
+
+handleIndexError ::
+  Member ConsoleIO r => Sem (Error CardDoesNotExist : r) () -> Sem r ()
+handleIndexError = (>>= either (const writeErrorMessage) pure) . runError
+  where
+    writeErrorMessage = writeln "error: unrecoverable index out of bounds error"
+
+playGame ::
+  forall p.
+  ( Enum p,
+    Bounded p,
+    Show p,
+    Ord p
+  ) =>
+  GameState p ->
+  Sem
+    [ Output (Turn p),
+      PlayerIO p,
+      Error GameOver,
+      HasGameState p,
+      Error CardDoesNotExist,
+      ConsoleIO
+    ]
+    Void ->
+  IO ()
+playGame startingState game =
+  game
+    & ignoreOutput @(Turn p) . outputTurnToConsoleIO @p
+    & runPlayerIOAsConsoleUI
+    & runGame startingState
+    & (=<<) printEndgameResult
+    & handleIndexError
+    & raiseUnder @(Embed (InputT IO))
+    & runConsoleIOAsInputT @IO "hanabi> "
+    & runM
+    & runInputT defaultSettings
 
 main :: IO ()
-main = pure ()
+main = do
+  startingState <- sample startingState
+  playGame @Player startingState (gameLoop @Player minBound)
