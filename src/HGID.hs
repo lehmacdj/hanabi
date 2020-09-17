bdb3435b8ccfabefdb94cf3b0fcf58651d4f53a0
Devin Lehmacher <lehmacdj@gmail.com>
2020-09-16 21:48:26 -0700

    implement HttpApiData

--- src/HGID.hs
+++ src/HGID.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HGID where
 
 import MyPrelude
+import Servant.API
 
 -- | ID for a game of hanabi.
 -- TODO: decide what the exact representation of this will be/implement method
 -- for generating random ids
 newtype HGID = HGID {unHGID :: String}
-  deriving (Show, Generic)
+  deriving (Show, Read, Generic)
+
+instance FromHttpApiData HGID where
+  parseQueryParam = maybeToRight "couldn't parse HGID" . readMay
+
+instance ToHttpApiData HGID where
+  toQueryParam = tshow
